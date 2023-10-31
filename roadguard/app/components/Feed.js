330a13b5f5f7db18314344536deeac6314d10bda
Akshat Gupta <akshatgtc@gmail.com>
2023-11-01 00:38:41 +0530

    akshat

--- roadguard/app/components/Feed.js
+++ roadguard/app/components/Feed.js
@@ -1,12 +1,46 @@
-import Share from "./Share"
-
-
+"use client";
+import Share from "./Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Post from "./Post.js";
 export default function Feed() {
+  const [posts, setPosts] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("/api/posts/getpost");
+        setPosts(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, [posts]);
+
+  console.log(posts);
+  
+  // const [likes, setLikes] = useState({});
+
+  // const likeHandler = (postId) => {
+  //   setLikes((prevLikes) => ({
+  //     ...prevLikes,
+  //     [postId]: !prevLikes[postId],
+  //   }));
+  // };
+
   return (
     <div className="flex-5.5">
-      <div className="pt-10 pl-20">
-        <Share/>
-      </div>
+        <div className="pt-15 pl-10">
+          <Share />
+        {
+          posts.map((p) => <Post key={p._id} p={p} />)
+        }
+          
+        </div>
+      
     </div>
-  )
+  );
 }
---
+++ roadguard/app/components/Post.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Post = ({p}) => {
+
+  const { username, photo, caption, profileImage } = p
+
+  return (
+    <>
+    <div className="postTop flex items-center justify-between">
+            <div className="postTopLeft flex items-center">
+              <img
+                className="postProfileImg w-8 h-8 rounded-full"
+                src={profileImage}
+                alt=""
+              />
+              <span className="postUsername text-lg font-semibold">
+                {username}
+              </span>
+              {/* <span className="postDate text-sm">{post.date}</span> */}
+            </div>
+            <div className="postTopRight">
+              <MoreVertIcon />
+            </div>
+          </div>
+          <div className="postCenter my-5">
+            <span className="postText">{caption}</span>
+            <img
+              className="postImg mt-5 w-full max-h-96 object-contain"
+              src={photo}
+              alt=""
+            />
+          </div>
+          <div className="postBottom flex items-center justify-between">
+            <div className="postBottomLeft flex items-center">
+              <img
+                // src={likes[post.id] ? "assets/heart.png" : "assets/like.png"}
+                alt=""
+                className="likeIcon w-6 h-6 cursor-pointer"
+                // onClick={() => likeHandler(post.id)}
+              />
+              <span className="postlikeCounter text-base">
+                {/* {likes[post.id] ? "You and others like it" : "Others like it"} */}
+              </span>
+            </div>
+            <div className="postBottomRight">
+              <span className="postCommentText cursor-pointer border-b border-dashed border-gray-400">
+                {/* {post.comment} comments */}
+              </span>
+            </div>
+          </div>
+          </>
+  )
+}
+
+export default Post
